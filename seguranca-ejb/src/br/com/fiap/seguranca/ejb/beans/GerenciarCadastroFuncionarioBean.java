53fc700290d515a964504f655698d49f5700a67f
leandro1604 <leandro1604@1cea0cac-4887-f46d-cf42-11d0077f9ff4>
2010-04-14 14:33:41 +0000

    Implemtação das regras de negócio e validação

--- seguranca-ejb/src/br/com/fiap/seguranca/ejb/beans/GerenciarCadastroFuncionarioBean.java
+++ seguranca-ejb/src/br/com/fiap/seguranca/ejb/beans/GerenciarCadastroFuncionarioBean.java
@@ -3,17 +3,22 @@
  */
 package br.com.fiap.seguranca.ejb.beans;
 
+import java.util.List;
+
 import javax.annotation.security.PermitAll;
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
+import javax.ejb.EJBTransactionRolledbackException;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateful;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.exception.ConstraintViolationException;
 import org.jboss.security.annotation.SecurityDomain;
 
+import br.com.fiap.seguranca.domain.entity.EntityBasic;
 import br.com.fiap.seguranca.domain.entity.Funcionario;
 import br.com.fiap.seguranca.domain.enums.PerfilFuncionario;
 import br.com.fiap.seguranca.ejb.dao.interfaces.FuncionarioDAO;
@@ -33,29 +38,100 @@ public class GerenciarCadastroFuncionarioBean extends BeanValidator implements C
 
 	@EJB
 	private FuncionarioDAO funcionarioDAO;
-	
+
 	private static final Log LOG = LogFactory.getLog(GerenciarCadastroFuncionarioBean.class);
-	
+
+	/*
+	 * (non-Javadoc)
+	 * @see br.com.fiap.seguranca.ejb.interfaces.local.CadastrarFuncionarioLocal#cadastrarFuncionario(br.com.fiap.seguranca.domain.entity.Funcionario)
+	 */
 	@Override
 	@PermitAll
 	public Funcionario cadastrarFuncionario(Funcionario funcionario)throws EJBException {
-		
-		LOG.info("Iniciando cadastro de funcionario");
-		
-		validator = new CadastrarFuncionarioValidator();
+
+		try{
+
+			validateEntity(funcionario.getSeguranca());
+
+			if(valid){
+
+				LOG.info("Cadastrando funcionario");
+				funcionario = funcionarioDAO.insert(funcionario);
+				
+				valid = true;
+				
+				return funcionario; 
+
+			}else{
+
+				LOG.info("Dados invalidos");
+				return funcionario;
+			}
+		}catch (EJBTransactionRolledbackException e) {
+			/**
+			 * Tratamento para duplicidade de login na base de dados
+			 */
+			if(e.getCausedByException().getCause().getClass().equals(ConstraintViolationException.class)){
+				messageValidator = "Login já está sendo utilizado por outro usuário.";
+				valid = false;
+				return funcionario;
+			}
+			else	
+				throw new EJBException(e);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw new EJBException(e);
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see br.com.fiap.seguranca.ejb.interfaces.local.CadastrarFuncionarioLocal#validarFuncionario(br.com.fiap.seguranca.domain.entity.Funcionario)
+	 */
+	@Override
+	@PermitAll
+	public boolean validarFuncionario(Funcionario funcionario) throws EJBException {
 
 		funcionario.setPerfil(PerfilFuncionario.FUNCIONARIO);
 
-		LOG.info("Validando dados do funcionario");
-		messageValidator = validator.validar(funcionario);
-		
+		validateEntity(funcionario);
+
 		if(messageValidator != null)
 			valid = false;
 		else
 			valid = true;
-		
-		LOG.info("Cadastrando funcionario");
-		return funcionarioDAO.insert(funcionario);
 
+		return valid;
+	}
+
+	/**
+	 * Valida as entidades de utilizadas nas regras de negocio
+	 * @param entity {@link EntityBasic}
+	 */
+	private <E extends EntityBasic> void validateEntity(E entity){
+
+		LOG.info("Iniciando validacao de dados da entidade : "+entity.getClass().getSimpleName());
+		validator = new CadastrarFuncionarioValidator();
+
+		LOG.info("Validando dados da entidades : "+entity.getClass().getSimpleName());
+		messageValidator = validator.validar(entity);
+
+		if(messageValidator != null)
+			valid = false;
+		else
+			valid = true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see br.com.fiap.seguranca.ejb.interfaces.local.CadastrarFuncionarioLocal#listarFuncionarios()
+	 */
+	@Override
+	@PermitAll
+	public List<Funcionario> listarFuncionarios() throws EJBException {
+		LOG.info("Listando funcionarios");
+		return funcionarioDAO.listarFuncionarios();
 	}
 }
--- seguranca-ejb/src/br/com/fiap/seguranca/ejb/interfaces/local/CadastrarFuncionarioLocal.java
+++ seguranca-ejb/src/br/com/fiap/seguranca/ejb/interfaces/local/CadastrarFuncionarioLocal.java
@@ -3,6 +3,8 @@
  */
 package br.com.fiap.seguranca.ejb.interfaces.local;
 
+import java.util.List;
+
 import javax.ejb.EJBException;
 import javax.ejb.Local;
 
@@ -22,5 +24,19 @@ public interface CadastrarFuncionarioLocal extends LocalValidator{
 	 * @throws EJBException
 	 */
 	public Funcionario cadastrarFuncionario(Funcionario funcionario)throws EJBException;
+
+	/**
+	 * Valida os dados informados para o cadastro de funcionario
+	 * @param funcionario {@link Funcionario}
+	 * @return {@link Boolean}
+	 * @throws EJBException
+	 */
+	public boolean validarFuncionario(Funcionario funcionario)throws EJBException;
 	
+	/**
+	 * Lista todos os funcionarios
+	 * @return {@link List} < {@link Funcionario} >
+	 * @throws EJBException
+	 */
+	public List<Funcionario> listarFuncionarios() throws EJBException;
 }
